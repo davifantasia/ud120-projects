24158886c8cc96992793862a092e9312fe9cfd31
Adewale Oluyinka <davifantasia@hotmail.com>
2020-06-18 11:56:01 +0200

    leasson 8 quiz 12 completed

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -15,6 +15,31 @@ def outlierCleaner(predictions, ages, net_worths):
 
     ### your code goes here
 
-    
+    # Get dict of errors
+    error_dict = {}
+
+    for index, pred in enumerate(predictions):
+        error_dict[index] = pred[0] - net_worths[index][0]
+
+    # Get integer of 10% of data points
+    ten_percent_count = int(len(predictions) * 0.1)
+
+    # Remove maxuimum residual errors ten_percent_count times
+    for i in range(ten_percent_count):
+        max_error = ('', 0)
+
+        # find the current maximum error value
+        for key, value in error_dict.iteritems():
+            if abs(value) > abs(max_error[1]):
+                max_error_list = list(max_error)
+                max_error_list[0] = key
+                max_error_list[1] = value
+                max_error = tuple(max_error_list)
+
+        del error_dict[max_error[0]]
+
+    for key, value in error_dict.iteritems():
+        cleaned_data.append((ages[key][0], net_worths[key][0], value))
+
     return cleaned_data
 
