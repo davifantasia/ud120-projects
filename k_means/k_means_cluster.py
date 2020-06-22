54a3a98640eb86ce589b6171e963ce29c6a99559
Adewale Oluyinka <davifantasia@hotmail.com>
2020-06-22 23:16:34 +0200

    lesson 9 complete

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,7 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
+# feature_3 = "total_payments"
+poi = "poi"
+# features_list = [poi, feature_1, feature_2, feature_3]
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
@@ -58,15 +60,26 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+# for f1, f2, _ in finance_features:
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = kmeans.predict(finance_features)
 
+array_of_exercised_stock_options = [value.get('exercised_stock_options') for key, value in data_dict.iteritems() if value.get('exercised_stock_options') != 'NaN']
+# print 'array_of_exercised_stock_options:', array_of_exercised_stock_options
+print 'maximum value taken by the exercised_stock_options:', max(array_of_exercised_stock_options)
+print 'minimum value taken by the exercised_stock_options:', min(array_of_exercised_stock_options)
 
-
+array_of_salary = [value.get('salary') for key, value in data_dict.iteritems() if value.get('salary') != 'NaN']
+# print 'array_of_exercised_stock_options:', array_of_exercised_stock_options
+print 'maximum value taken by the salary:', max(array_of_salary)
+print 'minimum value taken by the salary:', min(array_of_salary)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
