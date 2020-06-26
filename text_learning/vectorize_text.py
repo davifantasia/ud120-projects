a09a577349c354cf6b0ebc352a390a5e9800a3af
Adewale Oluyinka <davifantasia@hotmail.com>
2020-06-26 11:35:05 +0200

    lesson 11 complete

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -23,6 +23,28 @@ from parse_out_email_text import parseOutText
 """
 
 
+def remove_words(sentence, words_to_remove):
+    sentence_array = sentence.split()
+    # sentence_array = re.split("\W+", sentence)  # sentence array without quotations
+    filtered_array = [word for word in sentence_array if word.lower() not in words_to_remove]
+    sentence = ' '.join(filtered_array)
+    return sentence
+
+
+def test_remove_words():
+    sentence = 'since chris volunteered, he will replace sara and go to germani'
+
+    print 'remove words from:\n', sentence
+
+    words_to_remove = ["sara", "shackleton", "chris", "germani"]
+    sentence = remove_words(sentence, words_to_remove)
+
+    print 'sentence with removed words:\n', sentence
+
+
+# test_remove_words()
+
+
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
@@ -41,23 +63,60 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 200:
+        #     path = os.path.join('..', path[:-1])
+        #     print path
+        #     email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        #     ### use parseOutText to extract the text from the opened email
+        #     parsed_text = parseOutText(email)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        #     ### use str.replace() to remove any instances of the words
+        #     ### ["sara", "shackleton", "chris", "germani"]
+        #     parsed_text = remove_words(parsed_text, ["sara", "shackleton", "chris", "germani"])
 
-            ### append the text to word_data
+        #     # Remove stopwords
+        #     from nltk.corpus import stopwords
+        #     stop_words = stopwords.words("english")
+        #     parsed_text = remove_words(parsed_text, stop_words)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        #     ### append the text to word_data
+        #     word_data.append(parsed_text)
 
+        #     ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        #     if name == 'sara':
+        #         from_data.append(0)
+        #     if name == 'chris':
+        #         from_data.append(1)
 
-            email.close()
+        #     email.close()
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
+
+        ### use parseOutText to extract the text from the opened email
+        parsed_text = parseOutText(email)
+
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        parsed_text = remove_words(parsed_text, ["sara", "shackleton", "chris", "germani"])
+
+        # Remove stopwords
+        from nltk.corpus import stopwords
+        stop_words = stopwords.words("english")
+        parsed_text = remove_words(parsed_text, stop_words)
+
+        ### append the text to word_data
+        word_data.append(parsed_text)
+
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == 'sara':
+            from_data.append(0)
+        if name == 'chris':
+            from_data.append(1)
+
+        email.close()
 
 print "emails processed"
 from_sara.close()
@@ -66,10 +125,14 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
+print 'word_data[152]', word_data[152]
 
 
 
 ### in Part 4, do TfIdf vectorization here
 
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(word_data)
+print 'number of words in tf-ifd matrix:', len(vectorizer.get_feature_names())
+print 'word number 34597:', vectorizer.get_feature_names()[34596]
